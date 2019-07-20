5ec833023b6b3fb0de5f19deb72e2028a53dc731
johnwanjema <jonwanjema@gmail.com>
2019-07-20 12:12:24 +0300

    added code to display images

--- photos/views.py
+++ photos/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
+from .models import Image
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    images = Image.get_allImages()
+    return render(request, 'index.html',{"images":images})
