b3ad89f63cc2d6e78b36cd9c8b3999e40915ca75
johnwanjema <jonwanjema@gmail.com>
2019-07-20 11:06:54 +0300

    added classes and migrated

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+    
+
+class Location(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+    
+
+class Image(models.Model):
+    name = models.CharField(max_length=50)
+    photo = models.ImageField(upload_to='photos')
+    description = models.CharField(max_length=50)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, default='LOCATION')
+
+    def __str__(self):
+        return self.name
+
+
+
+    
+
+    
+    
