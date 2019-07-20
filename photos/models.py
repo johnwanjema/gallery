fd311579b21f732ede3ecd05304ed0bfcf903158
johnwanjema <jonwanjema@gmail.com>
2019-07-20 17:23:09 +0300

    added methods  for image and tested them

--- photos/models.py
+++ photos/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Category(models.Model):
     name = models.CharField(max_length=20)
 
@@ -9,7 +11,7 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
-    
+
 
 class Location(models.Model):
     name = models.CharField(max_length=20)
@@ -17,34 +19,54 @@ class Location(models.Model):
     def save_location(self):
         self.save()
 
-
     def __str__(self):
         return self.name
 
-    
 
 class Image(models.Model):
     name = models.CharField(max_length=50)
     photo = models.ImageField(upload_to='photos')
     description = models.CharField(max_length=50)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    location = models.ForeignKey(Location, on_delete=models.CASCADE, default='LOCATION')
-    
-    @classmethod
-    def get_allImages(cls):
-        images = cls.objects.all()
-        return images
+    location = models.ForeignKey(
+        Location, on_delete=models.CASCADE, default='LOCATION')
 
     def save_image(self):
         self.save()
+    
+    def delete_image(self):
+        self.delete()        
+        
 
+    @classmethod
+    def get_allImages(cls):
+        images = cls.objects.all()
+        return images  
+    
 
+    @classmethod
+    def update_image(cls):
+        pass
+
+    
+    @classmethod
+    def get_image_by_id(cls,id):
+        image_id = cls.objects.filter(pk = id)
+        return image_id
+
+    @classmethod
+    def search_image(cls,search_term):
+        images = cls.objects.filter(category__name__contains = search_term)
+        return images
+
+    @classmethod
+    def filter_by_location(cls):
+        pass
+
+   
     def __str__(self):
         return self.name
 
 
 
-    
 
-    
-    
--- photos/tests.py
+++ photos/tests.py
@@ -45,16 +45,24 @@ class ImageTestClass(TestCase):
         self.Camera= Category(name = 'Camera')
         self.Camera.save_category()
 
-        self.new_image= Image(name = 'Test image',photo = '1.jpg',description = 'test',catgory = self.Nairobi, location = self.Nairobi)
+        self.new_image= Image(name = 'Test image',photo = '1.jpg',description = 'test',category = self.Camera, location = self.Nairobi)
         self.new_image.save_image()
 
-    def get_allImages(self):
+    
+
+    def test_save_method(self):
         self.new_image.save_image()
-        images = Image.get_allImages()
+        images = Image.objects.all()
         self.assertTrue(len(images) > 0)
 
+    def test_delete_method(self):
+        self.new_image.delete_image
+        images = Image.objects.all()
+        self.assertTrue(len(images) > 0)
 
+   
 
+    
 
     def tearDown(self):
         Location.objects.all().delete()
