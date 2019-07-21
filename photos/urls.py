c6a5a0e76ba9e3a5036ae662b00d9062dd8017f5
johnwanjema <jonwanjema@gmail.com>
2019-07-21 14:05:34 +0300

    added urls for various locations

--- photos/urls.py
+++ photos/urls.py
@@ -6,6 +6,9 @@ from django.conf.urls.static import static
 urlpatterns=[
     url('^$',views.index,name = 'index'),
     url(r'^search/', views.search_results, name='search_results'),
+    url(r'^nairobi/', views.nairobi, name='nairobi'),
+    url(r'^rio/', views.rio, name='rio'),
+    url(r'^tokyo/', views.tokyo, name='tokyo'),
 ]
 
 if settings.DEBUG:
